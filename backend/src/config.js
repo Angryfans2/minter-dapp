350418e91c1629370ed0d98a1db90a0a9da57c63
Angryfans2 <100148250+Angryfans2@users.noreply.github.com>
2022-04-24 13:04:16 -0400

    update whitelisting

--- backend/src/config.js
+++ backend/src/config.js
@@ -64,7 +64,7 @@ const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0xbFfb14AFab1B67059718389308Fa5853f09c30f4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = [0xbFfb14AFab1B67059718389308Fa5853f09c30f4]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
